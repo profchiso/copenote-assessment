b60f951b5073ffec0401e593009b56a4f11a846e
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-08-22 09:49:27 +0100

    implement message route

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,7 +1,132 @@
 const Message = require('../database/models/Messages');
 
-exports.getMessages = async(req, res) => {}
-exports.getMessage = async(req, res) => {}
-exports.createMessage = async(req, res) => {}
-exports.updateMessage = async(req, res) => {}
-exports.deleteMessage = async(req, res) => {}
+exports.getAllMessages = async(req, res, ) => {
+    try {
+        let requestQueryObject = {...req.query }
+
+
+        let excludedQueryFields = ['sort', 'page', 'pageSize', 'fields']; //fields to exclude from the query
+        excludedQueryFields.forEach(
+            (element) => delete requestQueryObject[element]
+        ); //delete any key in the requestQueryObject containing an element in the  excludedQueryFields  array
+
+        //advance query using gte,lte,gt,lt
+        let queryToString = JSON.stringify(requestQueryObject);
+        queryToString = queryToString.replace(
+            /\b(gte|lte|gt|lt)\b/g,
+            (match) => `$${match}`
+        );
+
+        let parsedQuery = JSON.parse(queryToString);
+
+        let query = Message.find(parsedQuery); // the .select excludes any spacified field before sending the document
+
+        //sorting query result
+        if (req.query.sort) {
+            // to sort pass the sort param ie ?sort="field1,field2,..." //ascending
+            // to sort pass the sort param ie ?sort="-field1,-field2,..." //descending
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+
+
+        //field limiting
+        //pass a parameter called field eg. ?fields=field1,field2,...
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v ');
+        }
+
+        //pagination
+        //pass page and pageSize params  eg  ?page=1&pageSize=20
+        const page = req.query.page * 1 || 1;
+        const pageSize = req.query.pageSize * 1 || 50;
+        const skip = (page - 1) * pageSize;
+        query = query.skip(skip).limit(pageSize);
+
+        //handle a case where user specify page that does not exists
+        if (req.query.page) {
+            let numberOfDocument = await Message.countDocuments();
+            if (skip >= numberOfDocument) {
+                return res.status(404).json({ message: 'Page not found', statusCode: 404 });
+            }
+        }
+        //execute query
+        const result = await query; // query.sort().select().skip().limit()
+
+        res.status(200).json({ message: "Messages fetched  successfully", statusCode: 200, messages: result, sucess: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message || "something went wrong", statusCode: 500 })
+
+    }
+}
+exports.getMessage = async(req, res, ) => {
+    try {
+        const { id } = req.params;
+        const message = await Message.findById(id);
+        if (!message) {
+            return res.status(404).json({ message: 'Message not found', sucess: false, statusCode: 404 });
+        }
+        return res.status(200).json({ message, sucess: true, statusCode: 200 });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message || "something went wrong", statusCode: 500 })
+
+    }
+}
+exports.createMessage = async(req, res, ) => {
+    try {
+        const { message } = req.body;
+        if (!message) {
+            return res.status(400).json({ message: 'message is required', sucess: false, statusCode: 400 });
+        }
+        let messageId = await Message.estimatedDocumentCount();
+        const createdMessage = await Message.create({ message, messageId: messageId + 1 });
+        return res.status(201).json({ Message: createdMessage, sucess: true, statusCode: 201 });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message || "something went wrong", statusCode: 500 })
+    }
+}
+exports.updateMessage = async(req, res, ) => {
+    try {
+        const { id } = req.params;
+        const Message = await Subsciber.findById(id);
+        if (!Message) {
+            return res.status(404).json({ message: 'Message not found', sucess: false, statusCode: 404 });
+        }
+        if (req.body.messageId) {
+            delete req.body.messageId
+        }
+        const updatedMessage = await Subsciber.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+        return res.status(200).json({ Message: updatedMessage, sucess: true, statusCode: 200 });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message || "something went wrong", statusCode: 500 })
+
+    }
+}
+exports.deleteMessage = async(req, res, ) => {
+    try {
+        const { id } = req.params;
+        const Message = await Subsciber.findById(id);
+        if (!Message) {
+            return res.status(404).json({ message: 'Message not found', sucess: false, statusCode: 404 });
+        }
+        const updatedMessage = await Subsciber.findByIdAndDelete(id);
+        return res.status(204).json({ Message: updatedMessage, sucess: true, statusCode: 204 });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message || "something went wrong", statusCode: 500 })
+
+    }
+}
--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const { getAllMessages, getMessage, createMessage, updateMessage, deleteMessage } = require("../controllers/messages");
 const messagesRouter = express.Router();
+
+messagesRouter.get("/", getAllMessages);
+messagesRouter.get("/:id", getMessage);
+messagesRouter.post("/", createMessage);
+messagesRouter.put("/:id", updateMessage);
+messagesRouter.delete("/:id", deleteMessage);
 
 
 
