7e77cd6c51d9a6910d846e75e1d4ea55232c09f4
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-08-22 14:05:32 +0100

    fix some issues

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 
 
 // requre routes
-const subscribersRoutes = require('./routes/subscribers');
-const messagesRoutes = require('./routes/messages');
-const undefinedRoutes = require('./routes/undefinedRoutes');
-const dbToConnect = require('./database/dbconnection');
+const { subscribersRouter } = require('./routes/subscribers');
+const { messagesRouter } = require('./routes/messages');
+const { undefinedRouter } = require('./routes/undefinedRoutes');
+const connectToDb = require('./database/dbconnection');
 
 
 const app = express();
 app.use(cors())
 app.use(express.json());
-dbToConnect(); // connect to the database
+connectToDb(); // connect to the database
 
 app.get('/', (req, res) => {
 
-    res.status(200).json({ success: true, message: 'Welcome to the CopeNote API' });
+    res.status(200).json({ success: true, message: 'Welcome to the CopeNote API', statusCode: 200 });
 });
 
-app.use("/api/v1/subscribers", subscribersRoutes);
-app.use("/api/v1/messages", messagesRoutes);
-app.use(undefinedRoutes)
+app.use("/api/v1/subscribers", subscribersRouter);
+app.use("/api/v1/messages", messagesRouter);
+app.use(undefinedRouter)
 
 
 
