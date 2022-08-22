d6d2b8ea23ab56c6ebb4c7369633c0146c6449fe
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-08-22 02:57:57 +0100

    define controllers

---
+++ controllers/messages.js
@@ -0,0 +1,5 @@
+exports.getMessages = async(req, res) => {}
+exports.getMessage = async(req, res) => {}
+exports.createMessage = async(req, res) => {}
+exports.updateMessage = async(req, res) => {}
+exports.deleteMessage = async(req, res) => {}
---
+++ controllers/users.js
@@ -0,0 +1,5 @@
+exports.getAllUsers = async(req, res, ) => {}
+exports.getUser = async(req, res, ) => {}
+exports.createUser = async(req, res, ) => {}
+exports.updateUser = async(req, res, ) => {}
+exports.deleteUser = async(req, res, ) => {}
