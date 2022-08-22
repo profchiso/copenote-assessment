23f60759e818ca29054b6aafc794ed0b5f195e7b
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-08-22 04:03:33 +0100

    complete subscriber logic

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { getAllSubscribers, getSubscriber, createSubscriber, updateSubscriber, deleteSubscriber } = require("../controllers/subscribers");
 const subscribersRouter = express.Router();
 
-
+subscribersRouter.get("/", getAllSubscribers);
+subscribersRouter.get("/:id", getSubscriber);
+subscribersRouter.post("/", createSubscriber);
+subscribersRouter.put("/:id", updateSubscriber);
+subscribersRouter.delete("/:id", deleteSubscriber);
 module.exports = { subscribersRouter };
